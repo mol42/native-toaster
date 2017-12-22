7d4fd802010c845f3a3f1abae60c67952616f533
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2017-12-22 06:05:52 +0300

    various updates

--- android/src/main/java/com/mol42/workshop/RNNativeToasterPackage.java
+++ android/src/main/java/com/mol42/workshop/RNNativeToasterPackage.java
@@ -13,7 +13,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNNativeToasterPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNNativeToasterModule(reactContext));
+      return Arrays.<NativeModule>asList(new Toast(reactContext));
     }
 
     // Deprecated from RN 0.47
